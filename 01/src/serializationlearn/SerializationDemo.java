b9ef88a52418138fc12adf28bf3a810508c3886e
skskr <befree.in0@gmail.com>
2021-01-24 16:20:14 +0800

    Initial commit

--- 01/src/serializationlearn/SerializationDemo.java
+++ 01/src/serializationlearn/SerializationDemo.java
@@ -1,22 +1,16 @@
 package serializationlearn;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Vector;
+
 
 public class SerializationDemo {
     public static void main(String[] args) throws IOException {
-//        File file = new File("D:"+ File.separator + "person.ser");
-//        ObjectOutputStream oos = null;
-//        oos = new ObjectOutputStream(new FileOutputStream(file));
-//
-//        Person p = new Person("wang", 21);
-//        oos.writeObject(p);
-//        oos.close();
+        File file = new File("D:"+ File.separator + "person.ser");
+        ObjectOutputStream oos;
+        oos = new ObjectOutputStream(new FileOutputStream(file));
 
-        ArrayList arrayList = new ArrayList();
-        arrayList.ensureCapacity(100);
-        System.out.println(arrayList.size());
-        Vector vector = new Vector();
+        Person p = new Person("Hwang", 21);
+        oos.writeObject(p);
+        oos.close();
     }
 }
